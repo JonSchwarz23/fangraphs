801e140e769eedde36c05f73af41603c367ea384
Jonathan Schwarz <JonSchwarz23@gmail.com>
2023-11-21 16:09:27 -0500

    clean up clean up everybody do your share

--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { getBattingLeaders } from "./fangraphs";
--- src/test.ts
+++ src/test.ts
@@ -1,29 +1,12 @@
-import { getBatterScouting, getMajorLeagueLeaders, getPlayerInfo } from "./fangraphs";
-import { DataType, MaxNumberOfResults, Position, Stat, Team } from "./types";
 import { getMajorLeagueBattingLeaders, getMajorLeaguePitchingLeaders } from "./majorLeagueLeaders";
 
 const test = async () => {
-    // const result = await getMajorLeagueLeaders({
-    //     stats: Stat.Pitching,
-    //     dataType: DataType.Advanced,
-    //     season: 2023,
-    //     numberOfResults: MaxNumberOfResults,
-    //     inningsPitchedQualifier: "0",
-    // });
-
-    // for (const player of result) {
-    //     console.log(`${player.team}: ${player.name} - ${player.skillInteractiveEarnedRunAverage}`);
-    // }
-
-    // const playerInfo = await getPlayerInfo(result[0].id);
-    // console.log(playerInfo);
-
-    const pitchers = await getMajorLeagueBattingLeaders({
-        itemsPerPage: 5000,
+    const batters = await getMajorLeagueBattingLeaders({
+        itemsPerPage: 30,
         season: 2023,
-        minimumPlateAppearances: 0,
+        minimumPlateAppearances: 100,
     });
-    //console.log(pitchers);
+    console.log(batters);
 };
 
 test();
